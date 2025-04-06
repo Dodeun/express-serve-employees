f9e0f935ce319a1a96eb30592df0da531d7ba452
Dodeun <110058158+Dodeun@users.noreply.github.com>
2025-04-06 20:56:56 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const port = 3310;
 
 app.use(cors({ origin: ["http://localhost:5173"] }));
 
@@ -79,8 +80,6 @@ const getEmployees = (req, res) => {
 app.get("/api/employees", getEmployees);
 
 // Ecoute du port
-
-const port = 3310;
 
 app.listen(port, () => {
 	console.log(`Listerning app on port ${port}`);
